2cb8b58c4eea5cdedf078bb35047f1451c5874dd
woonyong.kr <woonyong.kr@gmail.com>
2026-09-03 21:59:29 +0900

    Updates

---
+++ static/scripts/script-8-51ee2d22.js
@@ -0,0 +1,6 @@
+
+      window.spaNavigate = (url, _) => window.location.assign(url)
+      window.addCleanup = () => {}
+      const event = new CustomEvent("nav", { detail: { url: document.body.dataset.slug } })
+      document.dispatchEvent(event)
+    
