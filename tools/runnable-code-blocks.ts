da81fd9d053f604b02d3e5e85cfb381184f67ede
choi woo-nyong <woonyong.kr@gmail.com>
2026-09-07 17:12:39 +0900

    fix: configure browser DOM adapter

--- tools/runnable-code-blocks.ts
+++ tools/runnable-code-blocks.ts
@@ -1,5 +1,9 @@
+import { configureDomAdapter } from "../vendor/runnable-code-blocks/src/dom";
 import { createRunnerRegistry } from "../vendor/runnable-code-blocks/src/runner-composition";
 import { enhanceRunnableCodeBlocks } from "../vendor/runnable-code-blocks/src/web-adapter";
+import { BROWSER_DOM_ADAPTER } from "../vendor/runnable-code-blocks/site/browser-dom-adapter";
+
+configureDomAdapter(BROWSER_DOM_ADAPTER);
 
 const registry = createRunnerRegistry({
   executionOrder: "private-first",
